c77e48f48e42b6926e00647a3bfe7ab2ca0a2803
Ivan Smirnov <gazovik7@gmail.com>
2025-12-16 00:33:56 +0300

    Call chromium.executablePath() when launching puppeteer

--- prerender.mjs
+++ prerender.mjs
@@ -65,7 +65,7 @@ async function prerender() {
     await waitForServer(URL, previewProcess);
 
     console.log('[prerender] Rendering in headless browser...');
-    const executablePath = await chromium.executablePath;
+    const executablePath = await chromium.executablePath();
     const browser = await puppeteer.launch({
       headless: chromium.headless,
       executablePath,
